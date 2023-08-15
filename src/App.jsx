bbc94fc11e7194c24cd131610293a8532b920ec7
Yoga Prambudi Sunaryudanto Putro <yogaprambudisp@gmail.com>
2023-08-15 13:28:12 +0700

    Woods Again

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/video-detail/:id" element={<VideoDetail />} />
+        <Route path="*" element={<h1>Not Found</h1>}  />
       </Routes>
 
 
