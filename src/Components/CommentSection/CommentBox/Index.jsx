02e00d26e03322ec7b367e6699ed433d2d021af7
Yoga Prambudi Sunaryudanto Putro <yogaprambudisp@gmail.com>
2023-08-15 13:01:44 +0700

    Fix

--- src/Components/CommentSection/CommentBox/Index.jsx
+++ src/Components/CommentSection/CommentBox/Index.jsx
@@ -1,13 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import userLogo1 from '../../../assets/UserLogos/1.png'
-import userLogo2 from '../../../assets/UserLogos/2.png'
-import userLogo3 from '../../../assets/UserLogos/3.png'
+
 // eslint-disable-next-line react/prop-types
 export default function CommentBox({ username, comment,userLogo }) {
-
-
-
-    
 
     return (
         <div className="comment-box">
