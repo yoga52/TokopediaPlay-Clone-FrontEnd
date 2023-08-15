e890121da009ab3e4c44cbd4cfe91b9a96600778
Yoga Prambudi Sunaryudanto Putro <yogaprambudisp@gmail.com>
2023-08-15 12:49:47 +0700

    YEs

--- src/Pages/VideoDetail/Index.jsx
+++ src/Pages/VideoDetail/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import ProductCard from '../../Components/ProductCard/Index'
 import VideoViewer from '../../Components/VideoViewer/Index'
 import CommentSection from '../../Components/CommentSection/Index'
